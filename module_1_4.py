03f93f8f8b1171f10d83dd2ee30e0bdfdd161fb6
1402pahom <1402pahom@mail.ru>
2024-09-01 20:56:07 +0700

    Add files via upload

--- module_1_4.py
+++ module_1_4.py
@@ -1,8 +1,8 @@
 doctors_surname=input("Введите фамилию доктора для записи: ")
-print("Вы записаны к доктору по фамилии, ", doctors_surname. upper())
-print("Вы записаны к доктору по фамилии, ". upper())
-print("Вы записаны к доктору по фамилии, ". lower())
-print("Вы записаны к доктору по фамилии, ". replace(' ', '' ))
+print('Количество введенных символов: ', (int(len(doctors_surname))))
+print("Вы записаны к доктору по фамилии, ", doctors_surname.upper())
+print("Вы записаны к доктору по фамилии, ", doctors_surname.lower())
+print("Вы записаны к доктору по фамилии, ",doctors_surname.replace(' ', '' ))
 print(doctors_surname[0])
 print(doctors_surname[-1])
 
